501ab4ca2ac96cfdaa7e246b8fae6a6fa6545ad6
unknown <lancejayme13@gmail.com>
2025-03-27 16:29:36 +0800

    Login: login  html updated

--- Login.php
+++ Login.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="d-flex justify-content-center align-items-center vh-100">
     <div class="container w-25">
