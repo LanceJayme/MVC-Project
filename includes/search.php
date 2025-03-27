413804409cc0a5eabe6e6a58be0d4f5b9ffc60e9
kean-diaz18 <keandiaz894@gmail.com>
2025-03-27 16:44:54 +0800

    Search: Search Function Updated

--- includes/search.php
+++ includes/search.php
@@ -2,3 +2,14 @@
 require_once "dbc.inc.php";
 
 $search = isset($_GET['search']) ? trim($_GET['search']) : '';
+
+try {
+    if (!empty($search)) {
+        $query = "SELECT * FROM college WHERE CollegeID LIKE ? OR CollegeName LIKE ? OR CollegeCode LIKE ?";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute(["%$search%", "%$search%", "%$search%"]);
+    } else {
+        $query = "SELECT * FROM college";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+    }
