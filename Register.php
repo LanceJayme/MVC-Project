55eb5302523eb61dcaa1a125993b2abf0d170dca
unknown <lancejayme13@gmail.com>
2025-03-27 16:28:33 +0800

    Register: register html updated

--- Register.php
+++ Register.php
@@ -3,6 +3,7 @@
 <head>
     <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body class="d-flex justify-content-center align-items-center vh-100">
     <div class="container w-25">
